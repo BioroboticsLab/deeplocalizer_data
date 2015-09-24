e6d4fac0dd37e07b5046daf5bda5a7e37b70c2aa
berleon <github@leon-sixt.de>
2015-09-24 22:52:11 +0200

    add configurable ratio of train to test set

--- scripts/split_train_test.py
+++ scripts/split_train_test.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
-
+from collections import defaultdict
 import sys
+import os
 
 with open(sys.argv[1], 'r') as f:
     filenames = f.readlines()
 
-lastdir = ''
 trainfiles = []
 testfiles = []
-for file in filenames:
-    splitted = file.split('/')
-    dir = splitted[len(splitted) - 2]
-    if dir != lastdir:
-        testfiles.append(file)
-    else:
-        trainfiles.append(file)
-    lastdir = dir
+test_to_train_ratio = 0.15
+dirnames = {os.path.dirname(f) for f in sorted(filenames)}
+for dirname in dirnames:
+    files_in_dir = [f for f in filenames if f.startswith(dirname)]
+    n = len(files_in_dir)
+    s = round(n*test_to_train_ratio)
+    trainfiles.extend(files_in_dir[s:])
+    testfiles.extend(files_in_dir[:s])
 
 for fname, fnamelist in [('train.txt', trainfiles), ('test.txt', testfiles)]:
     with open(fname, 'w') as f:
